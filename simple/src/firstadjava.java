b1a34e03ae687149c0f28f5198c23ccec0338862
MohdHamzaBeg <hamzabeg12june@gmail.com>
2022-02-18 19:55:59 +0530

    Changed file

--- simple/src/firstadjava.java
+++ simple/src/firstadjava.java
@@ -1,6 +1,6 @@
 public class firstadjava
 {
 public static void main(String[] args) {
-    System.out.println("It is time to make some projects");
+    System.out.println("It is time to make some projects!!!");
 }
 }
