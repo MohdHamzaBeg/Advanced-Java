feb08c49df5e47bdf5a1ed79945d6265d26b912c
MohdHamzaBeg <hamzabeg12june@gmail.com>
2022-02-19 19:49:08 +0530

    Old version of the code

--- simple/src/firstadjava.java
+++ simple/src/firstadjava.java
@@ -4,5 +4,6 @@ public class firstadjava
 public static void main(String[] args) {
     System.out.println("It is time to make some projects!!!");
     System.out.println("I am now in branch1 of the git");
+    System.out.println("This is the older version of my code");
 }
 }
